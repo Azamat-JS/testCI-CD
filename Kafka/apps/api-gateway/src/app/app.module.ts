7091f56de1f656da2319e50e986e5cda38f0eb69
Azamat-JS <azamatielts987@gmail.com>
2025-05-23 15:43:17 +0500

    practicing Kafka

---
+++ Kafka/apps/api-gateway/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { KAFKA_SERVICE } from './constants';
+
+@Module({
+  imports: [ClientsModule.register([
+    {
+      name: KAFKA_SERVICE,
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092']
+        }
+      }
+    }
+  ])],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ Kafka/apps/order-service/src/app/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getData() {
+    return this.appService.getData();
+  }
+
+  @EventPattern('order-created')
+  handleOrderCreated(@Payload() order:any){
+    console.log('Order received from api-gateway', order)    
+  }
+}
